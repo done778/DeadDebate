0f20ac7911f432611888dfb7b4dbfeb5ae363884
done778 <57587831+done778@users.noreply.github.com>
2025-10-25 15:55:30 +0900

    [feat] prototype

--- Assets/2. Scripts/GameManager.cs
+++ Assets/2. Scripts/GameManager.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
+    private Camera cm;
     public bool Playing { get; private set; }
+    public GameObject player;
+    public readonly float surviveTime = 600f;
 
     void Awake()
     {
-        if (Instance != null)
+        if (Instance != null && Instance != this)
         {
             Destroy(gameObject);
         }
@@ -18,6 +21,16 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+    }
+
+    void Start()
+    {
+        cm = Camera.main;
+    }
+    void Update()
+    {
+        if (Playing == true)
+            cm.transform.position = new Vector3(player.transform.position.x, cm.transform.position.y, player.transform.position.z - 18);
     }
 
     public void GameStart()
