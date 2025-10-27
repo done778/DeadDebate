ec4e47928ea80d252526ca855126238e47db3ce2
YoungSub <suba1001@daum.net>
2025-10-27 11:38:42 +0900

    [create] EnemyData
    scriptableobject enemydata 생성

---
+++ Assets/developer/youngsub/EnemyData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="createEnemyData", menuName ="data/enemy")]
+public class EnemyData : ScriptableObject
+{
+    public float healthPoint;
+    public float attakPower;
+    public float moveSpeed;
+    public float attackSpeed;
+}
