ad9c7a89f3aaae38779f9105f037f864ad681495
YoungSub <suba1001@daum.net>
2025-10-27 13:08:15 +0900

    [edit] EnemyData, [create] BaseEnemy
    [EnemyData]
    - 공격방식, 적 타입 선언(enum)
    - 사거리, 타입변수 추가
    - 깊은복사 추가
    
    [BaseEnemy]
    - 기본 적 데이터 생성

--- Assets/developer/youngsub/EnemyData.cs
+++ Assets/developer/youngsub/EnemyData.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName ="createEnemyData", menuName ="data/enemy")]
+public enum ATTAK_TYPE { 
+    None, //몸박만
+    Melee, //근접
+    Ranged,//원거리
+    Hybrid,//하이브리드
+}
+public enum ENEMY_TYPE
+{
+    Normal,
+}
+
+
+[CreateAssetMenu(fileName = "createEnemyData", menuName = "data/enemy")]
 public class EnemyData : ScriptableObject
 {
-    public float healthPoint;
-    public float attakPower;
+    public int healthPoint;
+    public int attakPower;
+    public int exp;
+
     public float moveSpeed;
     public float attackSpeed;
-    public int exp;
+    public float range;
+
+    public ENEMY_TYPE enemyType;
+    public ATTAK_TYPE attackType;
+
+   public EnemyData GetCopy(){
+        EnemyData copy = Instantiate(this);
+        return copy;
+   }
 }
