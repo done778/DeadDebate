91f1c62e6c1c2ea9a8c5c71434ddcdaa8633082c
YoungSub <suba1001@daum.net>
2025-10-27 15:17:13 +0900

    [feat] EnemySpawner
    보스 생성 메서드 추가

--- Assets/2. Scripts/EnemySpawner.cs
+++ Assets/2. Scripts/EnemySpawner.cs
@@ -5,37 +5,60 @@ using UnityEngine.Experimental.Rendering;
 
 public class EnemySpawner : MonoBehaviour
 {
-   
-
-    [Header("수치 변경을 위한 변수")]
+    //플레이어 트랜스폼 주입 추후 삭제 - 기즈모 용
     public Transform trs;
+
+    //스테이미 매니저로 옮겨야함
+    public Transform bossTrs;
+
     public float maxRadius;
     public int exclusionRadius = 10;
-    
+
     public List<GameObject> setEnemys = new List<GameObject>();
-    private Dictionary<ENEMY_TYPE,GameObject> prefabs = new Dictionary<ENEMY_TYPE, GameObject>();
+    private Dictionary<ENEMY_TYPE, GameObject> prefabs = new Dictionary<ENEMY_TYPE, GameObject>();
 
     private Vector3 createdPos;
-    private Vector3 spawnPos;
-    public GameObject curEnemy;
+    private GameObject curEnemy;
+    
+    public float timer;
 
     Coroutine spawnCoroutine = null;
+    Coroutine bossSpawnCoroutine = null;
     WaitForSeconds spawnDelay = new WaitForSeconds(1f);
 
     // Start is called before the first frame update
     void Start()
     {
         Initialize();
-        spawnCoroutine = StartCoroutine(EnemySpawn());
+        //spawnCoroutine = StartCoroutine(EnemySpawn());
+        bossSpawnCoroutine = StartCoroutine(BossSpawn());
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
     }
 
     private void Initialize()
     {
         //초기 세팅 프리팹리스트를 딕셔너리로 변경
-        foreach(var enemy in setEnemys){
+        foreach (var enemy in setEnemys)
+        {
             EnemyData data = enemy.GetComponent<EnemyController>().data;
             prefabs.Add(data.enemyType, enemy);
         }
+
+    }
+
+    //스테이지매니저에서 호출~
+    public void DoSpawnedEnemy()
+    {
+        spawnCoroutine = StartCoroutine(EnemySpawn());
+    }
+
+    public void DoSpawnedBoss()
+    {
+        bossSpawnCoroutine = StartCoroutine(BossSpawn());
     }
 
     IEnumerator EnemySpawn()
@@ -48,11 +71,34 @@ public class EnemySpawner : MonoBehaviour
             curEnemy = Instantiate(prefabs[ENEMY_TYPE.Normal], createdPos, Quaternion.identity);
             curEnemy.GetComponent<EnemyController>().Init(GameManager.Instance.player);
 
+            curEnemy.transform.SetParent(transform);
+
             yield return spawnDelay;
         }
     }
 
-    private Vector3 GetSpawnOffset(){
+    IEnumerator BossSpawn()
+    {
+        //보스 등장 전 처리할 로직들--------
+        //yield return WaitUntil(위에 로직이 다 될 동안 기다려)
+
+        //3분후 생성
+        yield return new WaitForSeconds(180f);
+        GameObject boss = Instantiate(prefabs[ENEMY_TYPE.Normal], bossTrs);
+        boss.GetComponent<EnemyController>().Init(GameManager.Instance.player, true);
+        boss.transform.localScale *= 5f;
+
+        boss.transform.SetParent(transform);
+    }
+
+    private void OnDestroy()
+    {
+        spawnCoroutine = null;
+        bossSpawnCoroutine = null;
+    }
+
+    private Vector3 GetSpawnOffset()
+    {
         float distance = Random.Range(exclusionRadius, maxRadius);
 
         float angle = Random.Range(0, 360f) * Mathf.Deg2Rad;
