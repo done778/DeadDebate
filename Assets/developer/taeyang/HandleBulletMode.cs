ef577354c25eb7b393babbf79cb142e188146a4e
gxvelxx <wolf2005k@naver.com>
2025-10-27 15:26:39 +0900

    [feat] HandleBulletMode.cs
    마우스 포인터 방향으로 발사하는 기능 추가

--- Assets/developer/taeyang/HandleBulletMode.cs
+++ Assets/developer/taeyang/HandleBulletMode.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class HandleBulletMode : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private TYBulletController bulletController;
+    [SerializeField] private float maxRayDistance = 100f; // 마우스 감지 거리
+    [SerializeField] private LayerMask aimLayerMask; // 포인터가 인식할 레이어(맵)
+
+    private void Start()
     {
-        
+        bulletController = GetComponent<TYBulletController>();
+
+        ShotBulletDirection();
     }
 
-    // Update is called once per frame
-    void Update()
+    //마우스 위치에 쏘는 함수
+    private void ShotBulletDirection()
     {
-        
+        Camera mainCam = Camera.main;
+
+        if (mainCam == null)
+        {
+            Debug.Log("메인카메라 없음");
+            return;
+        }
+
+        //마우스 위치에 Ray발사
+        Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, maxRayDistance, aimLayerMask))
+        {
+            //마우스가 가리키는 방향
+            Vector3 direction = hit.point - transform.position;
+
+            //땅 관통방지, XZ축으로만 일직선으로 쏘게
+            direction.y = 0f;
+
+            direction.Normalize();
+
+            transform.forward = direction;
+        }
+        else
+        {
+            //포인터 없으면 원래대로 앞으로 발사
+        }
     }
 }
