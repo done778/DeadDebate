dcf59fe2512d853e802af0ef73838db94a77a4a2
YoungSub <suba1001@daum.net>
2025-10-27 13:09:51 +0900

    [edit] EnemySpawner
    구조변경

--- Assets/2. Scripts/EnemySpawner.cs
+++ Assets/2. Scripts/EnemySpawner.cs
@@ -1,42 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 public class EnemySpawner : MonoBehaviour
 {
-    public GameObject Enemy;
-    public float radius;
+   
+
+    [Header("수치 변경을 위한 변수")]
+    public Transform trs;
+    public float maxRadius;
+    public int exclusionRadius = 10;
+    
+    public List<GameObject> setEnemys = new List<GameObject>();
+    private Dictionary<ENEMY_TYPE,GameObject> prefabs = new Dictionary<ENEMY_TYPE, GameObject>();
 
     private Vector3 createdPos;
     private Vector3 spawnPos;
-    private GameObject curEnemy;
+    public GameObject curEnemy;
 
+    Coroutine spawnCoroutine = null;
     WaitForSeconds spawnDelay = new WaitForSeconds(1f);
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(EnemySpawn());
+        Initialize();
+        spawnCoroutine = StartCoroutine(EnemySpawn());
+    }
+
+    private void Initialize()
+    {
+        //초기 세팅 프리팹리스트를 딕셔너리로 변경
+        foreach(var enemy in setEnemys){
+            EnemyData data = enemy.GetComponent<EnemyController>().data;
+            prefabs.Add(data.enemyType, enemy);
+        }
     }
 
     IEnumerator EnemySpawn()
     {
         while (GameManager.Instance.Playing)
         {
-            createdPos = Random.insideUnitCircle * radius;
-            spawnPos.x = createdPos.x + 10;
-            spawnPos.y = 0;
-            spawnPos.z = createdPos.y + 10;
-            createdPos = GameManager.Instance.player.transform.position + spawnPos;
-            curEnemy = Instantiate(Enemy, createdPos, Quaternion.identity);
+            //랜덤 위치 값에서 플레이어 중심에서 위치가 정해지도록
+            createdPos = GetSpawnOffset() + GameManager.Instance.player.transform.position;
+
+            curEnemy = Instantiate(prefabs[ENEMY_TYPE.Normal], createdPos, Quaternion.identity);
             curEnemy.GetComponent<EnemyController>().Init(GameManager.Instance.player);
 
             yield return spawnDelay;
         }
     }
 
+    private Vector3 GetSpawnOffset(){
+        float distance = Random.Range(exclusionRadius, maxRadius);
+
+        float angle = Random.Range(0, 360f) * Mathf.Deg2Rad;
+
+        float x = Mathf.Cos(angle) * distance;
+        float z = Mathf.Sin(angle) * distance;
+        return new Vector3(x, 0, z);
+    }
+
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireSphere(GameManager.Instance.player.transform.position, radius + 10);
+        Gizmos.DrawWireSphere(trs.position, maxRadius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(trs.position, exclusionRadius);
     }
 }
