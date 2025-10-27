2295ef194c86c8888875e015d7a400cef87c7b86
YoungSub <suba1001@daum.net>
2025-10-27 16:25:12 +0900

    [feat] Added Ranged Enemy Logic
    - 원거리 적 로직 기능 추가
    - 리팩토링

--- Assets/2. Scripts/EnemyController.cs
+++ Assets/2. Scripts/EnemyController.cs
@@ -1,16 +1,25 @@
+using System.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.PlasticSCM.Editor.WebApi;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     #region field
     public EnemyData data;
+    public GameObject bullet;
+    public Transform muzzle;
 
     //실제 수치들
     private EnemyData currentStat;
     public EnemyData CurrentStat => currentStat;
-    public bool isBoss = false;
 
-    private GameObject target;
+    private bool isBoss = false;
+    private bool isAttack = true;
+    private WaitForSeconds attackDelay;
+
+    private PlayerContoller target;
     #endregion
 
     private void Awake()
@@ -32,32 +41,45 @@ public class EnemyController : MonoBehaviour
     {
         if (GameManager.Instance.Playing)
         {
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                target.transform.position,
-                currentStat.moveSpeed * Time.deltaTime
-                );
             transform.LookAt(target.transform);
+
+            if (!IsTagetInRange() && isAttack)
+            {
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    target.transform.position,
+                    currentStat.moveSpeed * Time.deltaTime
+                    );
+            }
+
+            if (IsTagetInRange())
+            {
+                Attack();
+            }
         }
+
     }
 
     #region method
     public void Init(GameObject player, bool isBoss = false)
     {
-        target = player;
+        target = player.GetComponent<PlayerContoller>();
+        attackDelay = new WaitForSeconds(currentStat.attackSpeed);
 
+        currentStat = data.GetCopy();
         this.isBoss = isBoss;
     }
 
     public void Init(GameObject player, int changeValue, bool isBoss = false)
     {
-        target = player;
+        target = player.GetComponent<PlayerContoller>();
 
         currentStat.healthPoint = Mathf.RoundToInt(data.healthPoint * changeValue);
         currentStat.attakPower = Mathf.RoundToInt(data.attakPower * changeValue);
         currentStat.moveSpeed *= changeValue;
         currentStat.attackSpeed *= changeValue;
         currentStat.exp = Mathf.RoundToInt(data.exp * changeValue);
+        attackDelay = new WaitForSeconds(currentStat.attackSpeed);
 
         this.isBoss = isBoss;
     }
@@ -70,6 +92,70 @@ public class EnemyController : MonoBehaviour
             Die();
         }
     }
+
+
+    #region Attack Type
+    private void Attack()
+    {
+        if (!isAttack) return;
+
+        switch (currentStat.attackType)
+        {
+            case ATTAK_TYPE.None:
+                break;
+            case ATTAK_TYPE.Melee:
+                MeleeAttack();
+                break;
+            case ATTAK_TYPE.Ranged:
+                RangedAttack();
+                break;
+            case ATTAK_TYPE.Hybrid:
+                //HybridAttack();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void MeleeAttack()
+    {
+        StartCoroutine(AttackDelay(true));
+    }
+    private void RangedAttack()
+    {
+        SpawnProjectile();
+
+        StartCoroutine(AttackDelay(false));
+    }
+
+    private void SpawnProjectile()
+    {
+        GameObject newBullet = Instantiate(bullet, muzzle);
+        newBullet.transform.SetParent(null);
+    }
+
+    IEnumerator AttackDelay(bool isMelee)
+    {
+        isAttack = false;
+
+        if (isMelee)
+        {
+            target.TakeDamage(currentStat.attakPower);
+        }
+
+        yield return attackDelay;
+        isAttack = true;
+    }
+
+    private bool IsTagetInRange()
+    {
+        float distance = (target.transform.position - transform.position).sqrMagnitude;
+
+        return distance <= currentStat.range * currentStat.range;
+    }
+    #endregion
+
+
     private void Die()
     {
         target.GetComponent<PlayerContoller>().GetExp(currentStat.exp);
