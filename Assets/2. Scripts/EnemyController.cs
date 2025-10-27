ba238c550f849de55881a7cd2873601e58626f3d
YoungSub <suba1001@daum.net>
2025-10-27 13:09:36 +0900

    [edit] EnemyController
    구조 변경

--- Assets/2. Scripts/EnemyController.cs
+++ Assets/2. Scripts/EnemyController.cs
@@ -2,17 +2,21 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float moveSpeed;
-    public int maxHp;
-    GameObject target;
-    private int curHp;
-    private int exp;
-    // Start is called before the first frame update
-    void Start()
+    #region field
+    public EnemyData data;
+
+    //실제 수치들
+    private EnemyData currentStat;
+    public EnemyData CurrentStat => currentStat;
+
+    private GameObject target;
+    #endregion
+
+    private void Awake()
     {
-        exp = 1;
-        curHp = maxHp;
+        currentStat = data.GetCopy();
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Bullet"))
@@ -29,27 +33,41 @@ public class EnemyController : MonoBehaviour
         {
             transform.position = Vector3.MoveTowards(
                 transform.position,
-                target.transform.position, 
-                moveSpeed * Time.deltaTime
+                target.transform.position,
+                currentStat.moveSpeed * Time.deltaTime
                 );
             transform.LookAt(target.transform);
         }
     }
+
+    #region method
     public void Init(GameObject player)
     {
         target = player;
     }
+
+    public void Init(GameObject player, int changeValue)
+    {
+        target = player;
+        currentStat.healthPoint = Mathf.RoundToInt(data.healthPoint * changeValue);
+        currentStat.attakPower = Mathf.RoundToInt(data.attakPower * changeValue);
+        currentStat.moveSpeed *= changeValue;
+        currentStat.attackSpeed *= changeValue;
+        currentStat.exp = Mathf.RoundToInt(data.exp * changeValue);
+    }
+
     private void TakeDamage(int dmg)
     {
-        curHp -= dmg;
-        if (curHp <= 0) 
+        currentStat.healthPoint -= dmg;
+        if (currentStat.healthPoint <= 0)
         {
             Die();
         }
     }
     private void Die()
     {
-        target.GetComponent<PlayerContoller>().GetExp(exp);
+        target.GetComponent<PlayerContoller>().GetExp(currentStat.exp);
         Destroy(gameObject);
     }
+    #endregion
 }
