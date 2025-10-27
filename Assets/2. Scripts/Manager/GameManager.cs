06823f8707a44dc82ab851a77f071ec142df3d40
done778 <57587831+done778@users.noreply.github.com>
2025-10-27 20:34:30 +0900

    [feat] 상황 별 이벤트 선언

--- Assets/2. Scripts/Manager/GameManager.cs
+++ Assets/2. Scripts/Manager/GameManager.cs
@@ -1,17 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-    private Camera mainCamera;
+    
     public GameObject player;
+    private Camera mainCamera;
+    private Vector3 cameraPos;
     public readonly float surviveTime = 180f;
     private float playingTime;
-    private Vector3 cameraPos;
+
+    public event Action OnGameStart; // 게임 시작 이벤트
+    public event Action OnGameOver; // 게임 종료 이벤트
+    public event Action OnStageClear; // 스테이지 클리어 성공 이벤트
+    public event Action OnStageFailed; // 스테이지 클리어 실패 이벤트
+
+    // 한결님 UI 업데이트 메서드를 여기에 구독하세요.
+    public event Action OnTimerUpdate; // 1초마다 타이머 이벤트
+    public event Action OnKillCount; // 적 처치 시 이벤트
+    public event Action OnPlayerHpChange; // 플레이어 HP 변동 이벤트
+
+    private Coroutine timerUpdate;
 
     public bool Playing { get; private set; }
+
+    WaitForSeconds playTimer = new WaitForSeconds(1f);
 
     // 싱글톤
     void Awake()
@@ -48,43 +65,40 @@ public class GameManager : MonoBehaviour
             playingTime += Time.deltaTime;
             if (playingTime >= surviveTime)
             {
-                StageClear();
+                GameOver();
             }
         }
     }
-
     public void GameStart()
     {
         playingTime = 0f;
         Playing = true;
+        timerUpdate = StartCoroutine(PlayingTimer());
     }
 
     public void GameOver()
     {
         bool isClear = true;
         Playing = false;
+        StopCoroutine(timerUpdate);
         Time.timeScale = 0f;
         if (isClear)
         {
-            StageClear();
+            OnStageClear?.Invoke();
         }
         else
         {
-            StageFailed();
+            OnStageFailed?.Invoke();
         }
     }
 
-    // 스테이지 도전 성공
-    // 승리 관련 UI 띄우고 로비로 돌아가기
-    public void StageClear()
+    // 플레이어가 살아있는 동안 플레이타임 측정 코루틴
+    IEnumerator PlayingTimer()
     {
-
-    }
-
-    // 스테이지 도전 실패
-    // 패배 관련 UI 띄우고 로비로 돌아가기
-    public void StageFailed()
-    {
-
+        while (Playing)
+        {
+            OnTimerUpdate?.Invoke();
+            yield return playTimer;
+        }
     }
 }
