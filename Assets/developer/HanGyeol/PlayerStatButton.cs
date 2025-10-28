372f532ad705b0bd2900f1ebf93ed06b6d14ba95
done778 <57587831+done778@users.noreply.github.com>
2025-10-29 00:00:06 +0900

    [feat] 선택지 버튼 클릭 시 매개변수 담아서 이벤트

--- Assets/developer/HanGyeol/PlayerStatButton.cs
+++ Assets/developer/HanGyeol/PlayerStatButton.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerStatButton : MonoBehaviour
 {
-   
     public event Action<string> OnButtonClicked;
+    private string buttonType;
+    private Button button;
 
-    [SerializeField] private string buttonType;  
 
-    public void StatsButtonClick()
+    public void StatsButtonClick(int index)
     {
+        button = transform.GetChild(index).GetComponent<Button>();
+        buttonType = button.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text;
         OnButtonClicked?.Invoke(buttonType); 
-        Debug.Log("클릭 이벤트 발생!");
+        Debug.Log($"{buttonType}을 매개변수로 이벤트 발생함.");
     }
 }
 
